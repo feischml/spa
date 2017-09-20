0b4bf272f72c6d8a10458348f7d78ca45e9dd11e
feischml <metallaszlo@yahoo.com>
2017-09-20 22:47:54 +0300

    commit

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,5 @@
 export const constants = {
-    serverURL: "http://localhost:3100",
-    //reCaptchaToken: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" // localhost
-    reCaptchaToken: "6Le7GTAUAAAAAPruREDSHyjAr5o-DiTgWcp-JoBA" // productive firebase app
+    serverURL: "http://",
+    reCaptchaToken: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" // localhost
+    //reCaptchaToken: "6Le7GTAUAAAAAPruREDSHyjAr5o-DiTgWcp-JoBA" // productive firebase app
 }
