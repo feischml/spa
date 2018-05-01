fd14c80728698ab5e86c08eecf821015fac902c4
feischml <metallaszlo@yahoo.com>
2018-05-01 13:28:48 +0300

    commit

--- src/app/rankings/rankings.component.ts
+++ src/app/rankings/rankings.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { SmoothScrollToDirective } from 'ng2-smooth-scroll';
 
 @Component({
   selector: 'app-rankings',
   templateUrl: './rankings.component.html',
-  styleUrls: ['./rankings.component.css']
+  styleUrls: ['./rankings.component.css'],
+  providers: [ SmoothScrollToDirective ] 
 })
 export class RankingsComponent implements OnInit {
 
-  constructor(public translate: TranslateService) { }
+  constructor(public translate: TranslateService,
+              private myScroll: SmoothScrollToDirective = new SmoothScrollToDirective()) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    this.myScroll.scrollTo = "#rankings";
+    this.myScroll.offset = 80;
+    this.myScroll.onClick();
   }
 
 }
