3a3b80a9aab1d4ef99a001489c465f0d5e875dc8
feischml <metallaszlo@yahoo.com>
2018-05-03 18:52:41 +0300

    commit bugfix

--- src/app/coaching/coaching.component.ts
+++ src/app/coaching/coaching.component.ts
@@ -7,19 +7,20 @@ import { EmbedVideoService } from 'ngx-embed-video';
   selector: 'app-coaching',
   templateUrl: './coaching.component.html',
   styleUrls: ['./coaching.component.css'],
-  providers: [ SmoothScrollToDirective ]
+  providers: [SmoothScrollToDirective]
 })
 export class CoachingComponent implements OnInit {
 
   private iframe_html: any;
-  private youtubeUrl = "https://www.youtube.com/watch?v=7si_4wdoQAI";
+  private url = "https://www.youtube.com/watch?v=7si_4wdoQAI";
 
   constructor(private translate: TranslateService,
-              private myScroll: SmoothScrollToDirective = new SmoothScrollToDirective(),
-              private embedService: EmbedVideoService ) { }
+    private myScroll: SmoothScrollToDirective = new SmoothScrollToDirective(),
+    private embedService: EmbedVideoService) {
+    this.iframe_html = this.embedService.embed(this.url);
+  }
 
   ngOnInit() {
-    this.iframe_html = this.embedService.embed(this.youtubeUrl);
   }
 
   ngAfterViewInit(): void {
@@ -28,7 +29,7 @@ export class CoachingComponent implements OnInit {
     this.myScroll.onClick();
   }
 
-  private scrollToPlayer(vidId: string){
+  private scrollToPlayer(vidId: string) {
     this.myScroll.scrollTo = "#video-placeholder";
     this.myScroll.offset = 80;
     this.myScroll.onClick();
